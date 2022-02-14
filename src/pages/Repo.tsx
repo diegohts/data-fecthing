1db930e0c0ff59e02d8e167f53ba18fec391ce0c
Diego Henrique <diegohrp20@gmail.com>
2022-02-14 17:33:19 -0300

    It helps me to see the updated repository description. That is, it invalidates the cache time I put in the staleTime.

--- src/pages/Repo.tsx
+++ src/pages/Repo.tsx
@@ -1,4 +1,21 @@
+import { useQueryClient } from "react-query";
+import { useParams } from "react-router-dom"
 
 export function Repo() {
-  return <h1>Repo</h1>
+
+  const params = useParams() 
+  const currentRepository = params['*'] as string;
+
+    const queryClient = useQueryClient()
+
+  async function handleChangeRepositoryDescription(){
+    await queryClient.invalidateQueries(['repos'])
+  }
+
+  return ( 
+        <div>
+            <h1>{ currentRepository }</h1> 
+            <button onClick={handleChangeRepositoryDescription}>Alterar descrição</button>
+        </div>  
+    )
 }

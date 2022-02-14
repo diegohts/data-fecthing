ecd3b1d54dd5f6965ed9f249d8abaad4b24ec273
Diego Henrique <diegohrp20@gmail.com>
2022-02-14 15:53:35 -0300

    Installing and consuming the request with axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 type Repository ={
   full_name: string;
@@ -8,10 +9,9 @@ type Repository ={
 function App() {
   const [repositories, setRepositories] = useState<Repository[]>([])
   useEffect(() =>{
-    fetch('https://api.github.com/users/diegohts/repos')
-      .then(response => response.json())
-      .then(data => {
-        setRepositories(data);
+    axios.get('https://api.github.com/users/diegohts/repos')
+      .then(response => {
+        setRepositories(response.data);
       })
   }, [])
 
